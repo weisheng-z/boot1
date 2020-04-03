f7f26814bb9351fd1f57b3ef19e56d03f60e08c8
weisheng-z <840844894@qq.com>
2020-04-03 15:45:37 +0800

    添加test到git

---
+++ src/main/java/com/codefei/sys/entity/Sysuser.java
@@ -0,0 +1,10 @@
+package com.codefei.sys.entity;
+
+public class Sysuser {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
